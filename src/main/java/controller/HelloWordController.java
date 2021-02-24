9ae24b94e5bc17b7419070c5e8ab568b287f0cb9
nkligang <nkligang923@gmail.com>
2021-02-24 10:44:34 +0800

    Velocity

--- src/main/java/controller/HelloWordController.java
+++ src/main/java/controller/HelloWordController.java
@@ -5,11 +5,13 @@ import org.apache.velocity.VelocityContext;
 
 import com.fenglinga.tinyspring.framework.Controller;
 import com.fenglinga.tinyspring.framework.annotation.RequestMapping;
+import com.fenglinga.tinyspring.mysql.BaseObject;
 
 @com.fenglinga.tinyspring.framework.annotation.Controller
 public class HelloWordController extends Controller {	
 	@RequestMapping(value = "/hello_world.html", method = HttpMethod.GET)
 	public String hello_world(VelocityContext model) throws Exception {
+		model.put("Timestamp", BaseObject.date());
 		return "hello_world.html";
 	}
 }
